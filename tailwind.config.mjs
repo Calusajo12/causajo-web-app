5a0b642f2e3191ba0335170efc0f8e038ea4a93e
Yeiner99SD <yeinermoreno2005@gmail.com>
2025-06-02 18:09:38 -0500

    Add font dependencies and create initial component structure
    - Added @fontsource-variable/cinzel and @fontsource/ubuntu to package.json and pnpm-lock.yaml.
    - Created new Astro components: Banner, Card, Event, FormContact, Header, Testimonial.
    - Created new pages: aboutUs, allEvents, contact, notFoundPage.
    - Created new sections: About, Advantages, Contact, Events, Footer, Goals, Home, Services.
    - Updated Layout.astro to include font imports and modified styles.
    - Added tailwind.config.mjs for Tailwind CSS configuration.

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        cinzel: ['"Cinzel Variable"', 'serif'],
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+})
