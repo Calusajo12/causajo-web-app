8f3db6954b5d5360443fd9c949a47619ea77354e
Drivas04 <drivasb@unbosque.edu.co>
2025-06-06 18:49:00 -0500

    Agregar secciones de ventajas y servicios con sus respectivos íconos y descripciones; actualizar componentes para incluir nuevos datos.

---
+++ Advantages.ts
@@ -0,0 +1,22 @@
+import type { AdvantagesType } from "./AdvantagesType";
+import Experience from "./Experience.astro";
+import Personalization from "./Personalization.astro";
+import Quality from "./Quality.astro";
+
+export const advantages:AdvantagesType[] = [
+    {
+        title: "Experiencia",
+        description: "Más de 10 años organizando eventos exitosos.",
+        icon: Experience
+    },
+    {
+        title: "Personalización",
+        description: "Cada evento es único, adaptamos nuestros servicios a tus necesidades.",
+        icon: Personalization
+    },
+    {
+        title: "Calidad",
+        description: "Trabajamos con los mejores proveedores para garantizar la excelencia.",
+        icon: Quality
+    }
+];
